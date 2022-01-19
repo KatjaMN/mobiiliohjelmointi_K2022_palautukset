1b971b24a6e6a59f7298bdb4959cac6ae07f87a0
KatjaMN <katjamarianyman@gmail.com>
2022-01-19 22:18:56 +0200

    Kokeilu

--- App.js
+++ App.js
@@ -1,10 +1,31 @@
+import React, { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { Button, TextInput } from 'react-native';
+import { Alert } from 'react-native';
 
 export default function App() {
+
+
+  const buttonPressedAdd = () => { Alert.alert('')};
+  const buttonPressedSubtract = () => { Alert.alert('')};
+
+  const [text1, setText1] = useState('');
+  const [text2, setText2] = useState('');
+
+  const getResult = (text1, text2) => {
+    return text1 + text2
+  };
+
+  //const [number, onChangeNumber] = React.useState(null);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Result {getResult}</Text>
+      <TextInput style={styles.input} onChangeText={text1 => setText1(text1)} value={text1}/>
+      <TextInput style={styles.input} onChangeText={text2 => setText2(text2)} value={text2}/>
+      <Button onPress={buttonPressedAdd} title='+'/>
+      <Button onPress={buttonPressedSubtract} title='-'/>
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +38,10 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  input: {
+    width:100, 
+    borderColor: 'gray', 
+    borderWidth:1,
+  },
+
 });
